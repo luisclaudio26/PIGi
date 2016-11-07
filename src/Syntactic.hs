7ab7781feec602574c8be6020e1476b158ff4f0b
Lucas Torres <lucas.trr0@gmail.com>
2016-11-07 18:19:21 -0300

    Syntactic Expressions

--- src/Syntactic.hs
+++ src/Syntactic.hs
@@ -1,7 +1,9 @@
 module Syntactic where
 
+import Control.Monad.Identity
 import Text.Parsec (eof)
 import Text.Parsec.Prim
+import Text.Parsec.Expr
 import PosParsec
 import Lexical
 
@@ -51,15 +53,41 @@ synlitint = syntoken $
             _ -> Nothing
 
 
+data SynExpr = SynVal (Located SynIdent)
+             | SynPlus (Located SynExpr) (Located SynExpr)
+             | SynTimes (Located SynExpr) (Located SynExpr)
+             deriving (Show)
+
+synexprval :: SynParser SynExpr
+synexprval = locate $
+    do val <- synident
+       return $ SynVal val
+
+synexprop :: LexToken
+          -> (Located SynExpr -> Located SynExpr -> SynExpr)
+          -> Parsec [PosLexToken] () (Located SynExpr
+                                     -> Located SynExpr
+                                     -> Located SynExpr)
+synexprop opToken constr =
+    do tok <- synlex opToken
+       return $ \e1 e2 -> mklocated (getpos tok) $ constr e1 e2
+
+synoptable :: OperatorTable [PosLexToken] () Identity (Located SynExpr)
+synoptable = [[Infix (synexprop LexPlus SynPlus) AssocLeft],
+              [Infix (synexprop LexTimes SynTimes) AssocLeft]]
+
+synexpr :: SynParser SynExpr
+synexpr = buildExpressionParser synoptable synexprval
+
 -- !! EVERYTHING BELOW THIS LINE IS WRONG !!
 
 -- | Syntactic construct for module
-data SynModule = SynModule [Located SynIdent] deriving (Show)
+data SynModule = SynModule [Located SynExpr] deriving (Show)
 
 -- | SynParser for whole module
 synmodule :: SynParser SynModule
 synmodule = locate $
-    do ids <- many synident
+    do ids <- many synexpr
        eof
        return (SynModule ids)
 
