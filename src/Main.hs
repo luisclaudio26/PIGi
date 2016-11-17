14548e5aafd211ca2d471c9b24e54efb90ef11f2
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-11-17 15:56:27 -0300

    show symbol table

--- src/Main.hs
+++ src/Main.hs
@@ -26,7 +26,7 @@ runsynparser tokens filename =
 -- TODO: receive a list of SynModules, apply semModule to
 -- each one.
 runstaticanalyzer :: SynModule -> Either String SynModule
-runstaticanalyzer mod = Right mod -- semModule mod
+runstaticanalyzer mod = semModule mod
 
 printStaticAnalyzer :: Either String SynModule -> IO ()
 printStaticAnalyzer x = case x of
